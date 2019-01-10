92550a1734e7005e689e51a7a2d1479cdb12cbf2
swaroop sambhayya <swaroop.sambhayya@gmail.com>
2019-01-10 11:51:49 +0530

    new pro

---
+++ build/precache-manifest.8466b50520e1acea25c168e20d034bca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dd72ddcb4bce7b2540f6",
+    "url": "/static/js/main.dd72ddcb.chunk.js"
+  },
+  {
+    "revision": "66000b17c2d5481169ab",
+    "url": "/static/js/1.66000b17.chunk.js"
+  },
+  {
+    "revision": "dd72ddcb4bce7b2540f6",
+    "url": "/static/css/main.29f55ff2.chunk.css"
+  },
+  {
+    "revision": "66000b17c2d5481169ab",
+    "url": "/static/css/1.7272acbc.chunk.css"
+  },
+  {
+    "revision": "a9a122a1177276ea1523e06fff401cac",
+    "url": "/index.html"
+  }
+];
